0b70147c845f243953d6a1b723ddca917d309234
grch <samsobaka@gmail.com>
2023-02-28 01:15:57 +0400

    bot update in Typescript v1

--- src/bot.ts
+++ src/bot.ts
@@ -1,178 +1,162 @@
-import { Bot, InlineKeyboard, webhookCallback } from "grammy";
-import { chunk } from "lodash";
-import express from "express";
-import { applyTextEffect, Variant } from "./textEffects";
+import { Bot, Context, InlineKeyboard, SessionFlavor, webhookCallback } from "https://deno.land/x/grammy@v1.14.1/mod.ts";
+import { chunk } from 'https://raw.githubusercontent.com/lodash/lodash/4.17.21-es/lodash.js';
+import express from "npm:express@4.18.2";
+import { applyTextEffect, Variant } from "./textEffects.ts";
+import { setTimeout } from "node:timers/promises";
 
-import type { Variant as TextEffectVariant } from "./textEffects";
+import daily from "./data.json" assert { type: "json" }
+import breathe from "./breathe.json" assert { type: "json" }
+import neck from "./neck.json" assert { type: "json" }
+import back from "./intensive.json" assert { type: "json" }
+import office from "./office.json" assert { type: "json" }
+
+import type { Variant as TextEffectVariant } from "./textEffects.ts";
+
+interface SessionData {
+  favoriteIds: string[]
+}
+
+interface Day {
+  exercises: number[],
+  description: string[]
+}
+
+type MyContext = Context & SessionFlavor<SessionData>
 
 // Create a bot using the Telegram token
-const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
+const bot = new Bot("1172030999:AAFL8vuB8CK7EOyn7_dlBAcZfAv1qqiJHYQ");
+const sourceChatId = -1001398397423
 
 // Handle the /yo command to greet the user
 bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
 
-// Handle the /effect command to apply text effects using an inline keyboard
-type Effect = { code: TextEffectVariant; label: string };
-const allEffects: Effect[] = [
-  {
-    code: "w",
-    label: "Monospace",
-  },
-  {
-    code: "b",
-    label: "Bold",
-  },
-  {
-    code: "i",
-    label: "Italic",
-  },
-  {
-    code: "d",
-    label: "Doublestruck",
-  },
-  {
-    code: "o",
-    label: "Circled",
-  },
-  {
-    code: "q",
-    label: "Squared",
-  },
-];
+//Working forward pattern
 
-const effectCallbackCodeAccessor = (effectCode: TextEffectVariant) =>
-  `effect-${effectCode}`;
+bot.command("daily", async (ctx) => {
 
-const effectsKeyboardAccessor = (effectCodes: string[]) => {
-  const effectsAccessor = (effectCodes: string[]) =>
-    effectCodes.map((code) =>
-      allEffects.find((effect) => effect.code === code)
+  const day = getDayOfTheYear();
+  const length = daily.length;
+  const todaysNumber = day % length
+  const pack = daily.at(todaysNumber) as Day
+
+  for (let i = 0; i < pack.exercises.length; i++) {
+    await bot.api.forwardMessage(
+      ctx.chat.id,
+      sourceChatId,
+      pack.exercises[i],
     );
-  const effects = effectsAccessor(effectCodes);
-
-  const keyboard = new InlineKeyboard();
-  const chunkedEffects = chunk(effects, 3);
-  for (const effectsChunk of chunkedEffects) {
-    for (const effect of effectsChunk) {
-      effect &&
-        keyboard.text(effect.label, effectCallbackCodeAccessor(effect.code));
-    }
-    keyboard.row();
+    await bot.api.sendMessage(
+      ctx.chat.id,
+      pack.description[i]
+    );
+    await setTimeout(500)
   }
-
-  return keyboard;
-};
-
-const textEffectResponseAccessor = (
-  originalText: string,
-  modifiedText?: string
-) =>
-  `Original: ${originalText}` +
-  (modifiedText ? `\nModified: ${modifiedText}` : "");
-
-const parseTextEffectResponse = (
-  response: string
-): {
-  originalText: string;
-  modifiedText?: string;
-} => {
-  const originalText = (response.match(/Original: (.*)/) as any)[1];
-  const modifiedTextMatch = response.match(/Modified: (.*)/);
-
-  let modifiedText;
-  if (modifiedTextMatch) modifiedText = modifiedTextMatch[1];
-
-  if (!modifiedTextMatch) return { originalText };
-  else return { originalText, modifiedText };
-};
-
-bot.command("effect", (ctx) =>
-  ctx.reply(textEffectResponseAccessor(ctx.match), {
-    reply_markup: effectsKeyboardAccessor(
-      allEffects.map((effect) => effect.code)
-    ),
-  })
-);
-
-// Handle inline queries
-const queryRegEx = /effect (monospace|bold|italic) (.*)/;
-bot.inlineQuery(queryRegEx, async (ctx) => {
-  const fullQuery = ctx.inlineQuery.query;
-  const fullQueryMatch = fullQuery.match(queryRegEx);
-  if (!fullQueryMatch) return;
-
-  const effectLabel = fullQueryMatch[1];
-  const originalText = fullQueryMatch[2];
-
-  const effectCode = allEffects.find(
-    (effect) => effect.label.toLowerCase() === effectLabel.toLowerCase()
-  )?.code;
-  const modifiedText = applyTextEffect(originalText, effectCode as Variant);
-
-  await ctx.answerInlineQuery(
-    [
-      {
-        type: "article",
-        id: "text-effect",
-        title: "Text Effects",
-        input_message_content: {
-          message_text: `Original: ${originalText}
-Modified: ${modifiedText}`,
-          parse_mode: "HTML",
-        },
-        reply_markup: new InlineKeyboard().switchInline("Share", fullQuery),
-        url: "http://t.me/EludaDevSmarterBot",
-        description: "Create stylish Unicode text, all within Telegram.",
-      },
-    ],
-    { cache_time: 30 * 24 * 3600 } // one month in seconds
-  );
 });
+
+bot.command("breathe", async (ctx) => {
+
+  const day = getDayOfTheYear();
+  const length = breathe.length;
+  const todaysNumber = day % length
+  const pack = breathe.at(todaysNumber) as Day
+
+  for (let i = 0; i < pack.exercises.length; i++) {
+    await bot.api.forwardMessage(
+      ctx.chat.id,
+      sourceChatId,
+      pack.exercises[i],
+    );
+    await setTimeout(500)
+  }
+});
+
+bot.command("back", async (ctx) => {
+
+  const day = getDayOfTheYear();
+  const length = back.length;
+  const todaysNumber = day % length
+  const pack = back.at(todaysNumber) as Day
+
+  for (let i = 0; i < pack.exercises.length; i++) {
+    await bot.api.forwardMessage(
+      ctx.chat.id,
+      sourceChatId,
+      pack.exercises[i],
+    );
+    await setTimeout(500)
+  }
+});
+
+bot.command("neck", async (ctx) => {
+
+  const day = getDayOfTheYear();
+  const length = neck.length;
+  const todaysNumber = day % length
+  const pack = neck.at(todaysNumber) as Day
+  console.log("todays day ", todaysNumber)
+
+  for (let i = 0; i < pack.exercises.length; i++) {
+    console.log(pack.exercises[i])
+
+    await bot.api.forwardMessage(
+      ctx.chat.id,
+      sourceChatId,
+      pack.exercises[i],
+    );
+    await setTimeout(500)
+  }
+});
+
+bot.command("office", async (ctx) => {
+
+  const day = getDayOfTheYear();
+  const length = office.length;
+  const todaysNumber = day % length
+  const pack = office.at(todaysNumber) as Day
+
+  for (let i = 0; i < pack.exercises.length; i++) {
+    await bot.api.forwardMessage(
+      ctx.chat.id,
+      sourceChatId,
+      pack.exercises[i],
+    );
+    await setTimeout(500)
+  }
+});
+
+function getDayOfTheYear(): number {
+  const now = new Date();
+  const start = new Date(now.getFullYear(), 0, 0);
+  const diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+  var oneDay = 1000 * 60 * 60 * 24;
+  var day = Math.floor(diff / oneDay);
+  return day
+}
 
 // Return empty result list for other queries.
 bot.on("inline_query", (ctx) => ctx.answerInlineQuery([]));
 
-// Handle text effects from the effect keyboard
-for (const effect of allEffects) {
-  const allEffectCodes = allEffects.map((effect) => effect.code);
-
-  bot.callbackQuery(effectCallbackCodeAccessor(effect.code), async (ctx) => {
-    const { originalText } = parseTextEffectResponse(ctx.msg?.text || "");
-    const modifiedText = applyTextEffect(originalText, effect.code);
-
-    await ctx.editMessageText(
-      textEffectResponseAccessor(originalText, modifiedText),
-      {
-        reply_markup: effectsKeyboardAccessor(
-          allEffectCodes.filter((code) => code !== effect.code)
-        ),
-      }
-    );
-  });
-}
 
 // Handle the /about command
 const aboutUrlKeyboard = new InlineKeyboard().url(
-  "Host your own bot for free.",
-  "https://cyclic.sh/"
+  "в бабруйск жывотные",
+  "https://updaff.com/"
 );
 
 // Suggest commands in the menu
 bot.api.setMyCommands([
-  { command: "yo", description: "Be greeted by the bot" },
-  {
-    command: "effect",
-    description: "Apply text effects on the text. (usage: /effect [text])",
-  },
+  { command: "daily", description: "Спина + шея 10 дней" },
+  { command: "back", description: "Спина 7 дней" },
+  { command: "neck", description: "Шея 7 дней" },
+  { command: "office", description: "Упрежнения для офиса" },
+  { command: "breathe", description: "Дыхательные упражнения" },
 ]);
 
 // Handle all other messages and the /start command
-const introductionMessage = `Hello! I'm a Telegram bot.
-I'm powered by Cyclic, the next-generation serverless computing platform.
-
-<b>Commands</b>
-/yo - Be greeted by me
-/effect [text] - Show a keyboard to apply text effects to [text]`;
+const introductionMessage = `
+<b>ВАЖНО!</b>
+Этот бот создан для личного использования и не несёт цели
+распространения или получения материальной выгоды.`;
 
 const replyWithIntro = (ctx: any) =>
   ctx.reply(introductionMessage, {
@@ -180,21 +164,24 @@ const replyWithIntro = (ctx: any) =>
     parse_mode: "HTML",
   });
 
+
+
 bot.command("start", replyWithIntro);
-bot.on("message", replyWithIntro);
+// bot.on("message", processCommand);
 
 // Start the server
-if (process.env.NODE_ENV === "production") {
-  // Use Webhooks for the production server
-  const app = express();
-  app.use(express.json());
-  app.use(webhookCallback(bot, "express"));
+// if (process.env.NODE_ENV === "production") {
+//   // Use Webhooks for the production server
+//   const app = express();
+//   app.use(express.json());
+//   app.use(webhookCallback(bot, "express"));
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
-  });
-} else {
+//   const PORT = 3000;
+//   app.listen(PORT, () => {
+//     console.log(`Bot listening on port ${PORT}`);
+//   });
+// } else {
+if (true) {
   // Use Long Polling for development
   bot.start();
 }
